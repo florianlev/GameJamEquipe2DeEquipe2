d4af99d959cb4fd3311e630219e2bd4d850e3ed4
Florian <flolevy33@gmail.com>
2019-01-15 17:38:10 -0500

    Create class enemy and children

---
+++ GameJamEquipe2/Assets/scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+
+    public int speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GameJamEquipe2/Assets/scripts/Wolf.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wolf : Enemy
+{
+    // Start is called before the first frame update
+
+    public int speed = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
