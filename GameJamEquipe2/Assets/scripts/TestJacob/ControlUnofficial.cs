2500d371a18e1a78b5ad8bcbf3281fe7ec5e46e9
Jacob Murray <jaymur2010@gmail.com>
2019-01-15 17:34:45 -0500

    Debut Gestion du stress

---
+++ GameJamEquipe2/Assets/scripts/TestJacob/ControlUnofficial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlUnofficial : MonoBehaviour
+{
+    public int movementSpeeed = 5;
+    CharacterController CC;
+    // Start is called before the first frame update
+    void Start()
+    {
+       CC = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MovX = Input.GetAxis("Horizontal");
+        float MovZ = Input.GetAxis("Vertical");
+
+        Vector3 speed = new Vector3(MovX * movementSpeeed, 0, MovZ * movementSpeeed);
+
+        CC.Move(speed * Time.deltaTime);
+    }
+}
---
+++ GameJamEquipe2/Assets/scripts/TestJacob/StressManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StressManager : MonoBehaviour
+{
+    public float CurrentStress = 0;
+    public float MaxStress = 100;
+    public bool IsStressing = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsStressing = true;
+    }
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsStressing)
+        {
+            StressIncrease(1);
+        }
+    }
+    public void StressIncrease(float NewStress)
+    {
+        CurrentStress += NewStress;
+        if (CurrentStress >= MaxStress)
+            GameOver();
+
+    }
+    public void GameOver()
+    {
+
+    }
+
+}
