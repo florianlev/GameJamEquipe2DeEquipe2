963d304e5a4d48e9e2c003529fd19e942480bbc5
Florian <flolevy33@gmail.com>
2019-01-15 17:13:19 -0500

    mouvement perso

--- GameJamEquipe2/Assets/scripts/PlayerControl.cs
+++ GameJamEquipe2/Assets/scripts/PlayerControl.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+
+    private Vector3 movementVector;
+
+    public string horizontalCtrl = "Horizontal_P1";
+    public string verticalCtrl = "Vertical_P1";
+
+    private CharacterController characterController;
+
+    public float movementSpeed = 15;
+
+    private float gravity = 40;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Mouvement du perso
+
+        movementVector.x = Input.GetAxis(horizontalCtrl) * movementSpeed;
+        movementVector.z = Input.GetAxis(verticalCtrl) * movementSpeed;
+
+
+        movementVector.y -= gravity * Time.deltaTime;
+
+        characterController.Move(movementVector * Time.deltaTime);
+
     }
 }
