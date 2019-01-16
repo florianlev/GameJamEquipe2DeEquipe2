628f61a68191ad45feb801d1c945f938be3a841c
Jacob Murray <jaymur2010@gmail.com>
2019-01-15 20:58:01 -0500

    Changement mineur sur le timer

--- GameJamEquipe2/Assets/scripts/TestJacob/Timer.cs
+++ GameJamEquipe2/Assets/scripts/TestJacob/Timer.cs
@@ -12,7 +12,7 @@ public class Timer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        TimeText = GetComponent<Text>();
+        TimeText = GetComponentInChildren<Text>();
     }
 
     // Update is called once per frame
