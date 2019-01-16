53dc62794903980247b3a25feedf3b0ff2b7aef2
Florian <flolevy33@gmail.com>
2019-01-15 19:48:46 -0500

    AI de base

---
+++ GameJamEquipe2/Assets/scripts/AI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AI : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if(_navMeshAgent == null)
+        {
+            Debug.LogError("Le nav mesh n'est pas attacher a : " + gameObject.name);
+        }
+        else
+        {
+            setDestination();
+        }
+    }
+
+    private void setDestination()
+    {
+        if (_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+    }
+
+}
