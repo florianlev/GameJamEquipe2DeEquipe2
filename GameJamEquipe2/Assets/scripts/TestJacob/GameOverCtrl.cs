1dc1378427d961957beb94f946611462cbccd46b
Jacob Murray <jaymur2010@gmail.com>
2019-01-16 13:17:55 -0500

    GameOverFinit
    GameOver Fonctionnel

--- GameJamEquipe2/Assets/scripts/TestJacob/GameOverCtrl.cs
+++ GameJamEquipe2/Assets/scripts/TestJacob/GameOverCtrl.cs
@@ -1,21 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOverCtrl : MonoBehaviour
 {
     private Timer timer;
+    public Text timeText;
+    private int minutes;
+    private int secondes;
     // Start is called before the first frame update
     void Start()
     {
         timer = FindObjectOfType<Timer>();
+        
         if (timer == null)
             Debug.Log("How di you get there ?!?!?!?");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        timeText.text = "Your client Survived : " + timer.minutes + " : " + timer.seconds;
+        Destroy(timer.gameObject);
+
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
