08862df2ffe640839887099a0c2db2d18a3c9cb8
Jacob Murray <jaymur2010@gmail.com>
2019-01-16 15:03:31 -0500

    Quelques SpawnPoints pour Fantômes et loup garou

---
+++ GameJamEquipe2/Assets/scripts/TestJacob/SpawnerManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    public List<GameObject> ghostSpawns;
+    public List<GameObject> wolfSpawns;
+    public List<GameObject> vampireSpawns;
+
+    public float initialWolfSpawnTime = 120;
+    public float initialVampireSpawnTime = 240;
+
+    public float ghostMinSpawnTime = 2;
+    public float ghostMaxSpawnTime = 4;
+
+    public float wolfMinSpawnTime = 2;
+    public float wolfMaxSpawnTime = 4;
+
+    public float vampireMinSpawnTime = 2;
+    public float vampireMaxSpawnTime = 4;
+
+
+    public GameObject ghostPrefab;
+    public GameObject wolfPrefab;
+    public GameObject vampirePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        StartCoroutine(spawnGhost(5));
+        StartCoroutine(spawnWolf(initialWolfSpawnTime));
+        StartCoroutine(spawnVampire(initialVampireSpawnTime));
+    }
+
+    private IEnumerator spawnGhost(float a_Delay)
+    {
+        yield return new WaitForSeconds(a_Delay);
+        float t_NewDelay = Random.Range(ghostMinSpawnTime, ghostMaxSpawnTime);
+        int t_SpawnPoint = Random.Range(0, ghostSpawns.Count);
+        Instantiate(ghostPrefab, ghostSpawns[t_SpawnPoint].transform.position, Quaternion.identity);
+
+        StartCoroutine(spawnGhost(t_NewDelay));
+    }
+    private IEnumerator spawnWolf(float a_Delay)
+    {
+        yield return new WaitForSeconds(a_Delay);
+
+        float t_NewDelay = Random.Range(wolfMinSpawnTime, wolfMaxSpawnTime);
+        int t_SpawnPoint = Random.Range(0, wolfSpawns.Count);
+        
+        Instantiate(wolfPrefab, wolfSpawns[t_SpawnPoint].transform.position, Quaternion.identity);
+
+        StartCoroutine(spawnWolf(t_NewDelay));
+    }
+    private IEnumerator spawnVampire(float a_Delay)
+    {
+        yield return new WaitForSeconds(a_Delay);
+        float t_NewDelay = Random.Range(ghostMinSpawnTime, ghostMaxSpawnTime);
+        int t_SpawnPoint = Random.Range(0, ghostSpawns.Count);
+        Instantiate(ghostPrefab, ghostSpawns[t_SpawnPoint].transform.position, Quaternion.identity);
+
+        StartCoroutine(spawnGhost(t_NewDelay));
+    }
+
+}
