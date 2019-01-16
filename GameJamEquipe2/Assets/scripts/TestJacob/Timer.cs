03a19031c20831f0f2c6bfe36d041873bd2356d4
Jacob Murray <jaymur2010@gmail.com>
2019-01-15 20:40:16 -0500

    Timer Fonctionnel

--- GameJamEquipe2/Assets/scripts/TestJacob/Timer.cs
+++ GameJamEquipe2/Assets/scripts/TestJacob/Timer.cs
@@ -6,18 +6,23 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public float Survivaltime = 0;
+    public int seconds = 0;
+    public int minutes = 0;
     public Text TimeText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimeText = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Survivaltime += Time.deltaTime;
+        minutes = (int)Survivaltime / 60;
+        seconds = (int)Survivaltime % 60;
 
+        TimeText.text = "Time : " + minutes + " : " + seconds;
 
     }
 }
