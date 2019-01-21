e57f29577e35c9a6b8769cf48bfdf7763ddd722e
Florian <flolevy33@gmail.com>
2019-01-21 17:13:39 -0500

    story + credits

--- GameJamEquipe2/Assets/scripts/MenuController.cs
+++ GameJamEquipe2/Assets/scripts/MenuController.cs
@@ -2,18 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+using UnityEngine.UI;
+
 
 public class MenuController : MonoBehaviour
 {
+    public GameObject controlsMenu;
+    public GameObject mainMenu;
+    public GameObject creditsMenu;
+    public GameObject logo;
+    public GameObject story;
+    private Button previousButton;
+
+    private bool inMainMenu = true;
+    private bool inStory = true;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("cancel") && !inMainMenu)
+        {
+            controlsMenu.gameObject.SetActive(false);
+            creditsMenu.gameObject.SetActive(false);
+            mainMenu.gameObject.SetActive(true);
+        }
+
+        if(Input.GetButtonDown("Take_P1") && inStory)
+        {
+            inStory = false;
+            mainMenu.gameObject.SetActive(true);
+            logo.gameObject.SetActive(true);
+            story.gameObject.SetActive(false);
+
+        }
+    }
+
+    public void OnControlMenu()
+    {
+        inMainMenu = false;
+
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("MainScene");
     }
 
-    public void controls()
+    public void QuitGame()
     {
-        SceneManager.LoadScene("MainScene");
-
+        Application.Quit();
     }
+
+
 
 }
