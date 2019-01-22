292932fd068501157efb3fffa57f9ea94114ba12
britney spears <m,nmjlk>
2019-01-22 01:53:40 -0500

    Cinematic

---
+++ GameJamEquipe2/Assets/cinematiqueTransition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cinematiqueTransition : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(deleteAube());
+    }
+
+    // Update is called once per frame
+
+
+    IEnumerator deleteAube()
+    {
+        yield return new WaitForSeconds(5.73f);
+        Destroy(this.gameObject);
+
+    }
+}
